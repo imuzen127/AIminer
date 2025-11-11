b9883839e1b140f96c4bcf29a9c76920414ee1e1
imuzen127 <imuzen127@users.noreply.github.com>
2025-11-11 10:02:10 +0900

    Initial commit: Setup AIminer Minecraft bot plugin project

---
+++ src/main/java/plugin/midorin/info/aIminer/AIminer.java
@@ -0,0 +1,17 @@
+package plugin.midorin.info.aIminer;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AIminer extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
